29be96375e3c142ca46ead2f18ae2fb2074c41ed
Kirill Aristov <kirillaristov14@yandex.ru>
2025-08-13 15:07:43 +0300

    done game tic_tac_toes

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -16,6 +16,25 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
+
+    def check_win():
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+    
+        return False
+
     def __str__(self):
         return (
             'Объект игрового поля размером '
