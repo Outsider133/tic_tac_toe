1cfc8852c4f0e427873094d829f337a55bcacc00
Kirill Aristov <kirillaristov14@yandex.ru>
2025-08-13 15:13:14 +0300

    fixed parts.py

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -28,7 +28,14 @@ class Board:
         # Иначе - ничья!
         return True
 
-    def check_win():
+    def check_win(self, player):
+        # Проверка по горизонталям и вертикалям
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+    
+        # Проверка по диагоналям
         if (all([self.board[i][i] == player for i in range(self.field_size)]) or
                 all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
             return True
